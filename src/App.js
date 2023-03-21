2e2d6727d3e58b89ae84468c88d39d604b0454e7
sinhayash9546 <sinhayash9546@gmail.com>
2023-03-21 12:00:59 +0530

    Added route for contact us

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import './App.css';
+import React from "react";
 
 import Header from "../src/components/Header"
 import Home from "../src/components/Home"
 import Coins from "../src/components/Coins"
 import Exchanges from "./components/Exchanges"
 import CoinDetails from "../src/components/CoinDetails"
+// import contactUs from "../src/components/ContactUs"
+import ContactUs from "../src/components/ContactUs"
 import Footer from "./components/Footer";
 
+
+
 function App() {
+
+
   return (
     <Router>
         
@@ -20,7 +27,7 @@ function App() {
         <Route path="/coins" element={<Coins />}/>
         <Route path="/exchanges" element={<Exchanges />}/>
         <Route path="/coins/:id" element={<CoinDetails />}/>
-
+        <Route path="/contactus" element = {<ContactUs />} />
       </Routes>
      
 
